74fbe3bbece9077112f8265681393567f07b65e5
NicoHood <git@nicohood.de>
2020-12-07 16:34:00 +0100

    Composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c9eaf03b50417a2ea52d86b4a8294f26af19f3e7',
+    'name' => 'nicohood/todo',
+  ),
+  'versions' => 
+  array (
+    'nicohood/todo' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c9eaf03b50417a2ea52d86b4a8294f26af19f3e7',
+    ),
+  ),
+);
